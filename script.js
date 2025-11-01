08b24c8b845c8784b988a122c34b1f381574256d
IFA-01 <bogatyr.oleg@yahoo.com>
2025-11-01 20:02:59 +0300

    commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const input = document.getElementById('textInput');
+    const button = document.getElementById('addButton');
+    const block = document.querySelector('.div1');
+
+    if (!input || !button || !block) {
+        console.log('Не найдены asdfaнеобходимые элементы:');
+        if (!input) console.log('- textInput');
+        if (!button) console.log('- addButton');
+        if (!block) console.log('- .div1');
+        return;
+      }
+
+    function updateButtonVisibility(){ 
+        button.style.display = input.value.trim() ? 'block' : 'none';    
+      }
+
+    function addTextBlock() {
+        const text = input.value.trim();
+        if (!text) return; 
+        const listElem = document.createElement('p');
+        listElem.textContent = text;
+        block.append(listElem);
+
+        input.value = '';
+        manageParagraphs();
+    }
+
+    function manageParagraphs(){
+        const allParagraphs = document.querySelectorAll('p')
+        const maxParagraphs = 5;
+        console.log(allParagraphs.length);
+        console.log(maxParagraphs);
+        if (allParagraphs.length > maxParagraphs) { allParagraphs[0].remove();}
+    }
+
+        updateButtonVisibility();
+        manageParagraphs();
+
+        input.addEventListener('input', updateButtonVisibility);
+        button.addEventListener('click', addTextBlock);
+});
